92ee1100c35d19cbe28aae398cd29c44c486c4ae
Michael_Marcellino <michaelarceus96@gmail.com>
2021-10-21 20:56:29 +0700

    fix html

--- src/user/routes.js
+++ src/user/routes.js
@@ -3,9 +3,10 @@ const express = require('express')
 const router = express.Router();
 
 router.get('/', (req,res) => {
-    res.status(200).json({
-        message: "Success GET User"
-    })
+    // res.status(200).json({
+    //     message: "Success GET User"
+    // })
+    res.sendFile('./index.html',{ root: 'src' });
 })
 
 module.exports = router
